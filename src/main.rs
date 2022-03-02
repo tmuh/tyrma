7e3d40bc24dff2009829b2364854355cc0d5d970
tmuh <teemu.heittola@gmail.com>
2022-03-02 17:47:28 +0200

    Add basic bevy setup.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use bevy::{prelude::*};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0., 0., 0.)))
+        .insert_resource(WindowDescriptor {
+            title: "Tyrmä".to_string(),
+            vsync: true,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn_bundle(UiCameraBundle::default());
 }
